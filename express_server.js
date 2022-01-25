8448a4f3f107e1ddc3245c0604f33a3bc6d3e818
Logan Woolf <loganwoolf@gmail.com>
2022-01-25 23:42:04 +0000

    Add cookie parser middleware to server

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 // use res.render to load up and ejs view file from ./views
 
 const PORT = 3000;
