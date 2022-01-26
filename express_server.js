972f761dc333851ed749469dce8c4b3e86049838
Logan Woolf <loganwoolf@gmail.com>
2022-01-26 00:37:39 +0000

    Enable login and logout
    Dynamically display login form in header of all pages based on cookie

--- express_server.js
+++ express_server.js
@@ -81,6 +81,12 @@ app.post('/login', (req, res) => {
   res.redirect('/urls');
 });
 
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+
+  res.redirect('/urls');
+});
+
 app.get('/urls/new', (req, res) => {
   const templateVars = {
     username: req.cookies.username,
